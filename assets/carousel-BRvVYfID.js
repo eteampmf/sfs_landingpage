073bfa1100fce34899251182b739055c283bf16f
ouwais_local <ouwais@amazon.com>
2025-09-14 00:32:08 +0200

    Updates

--- assets/carousel-BRvVYfID.js
+++ assets/carousel-BRvVYfID.js
@@ -1,4 +1,4 @@
-import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-BLvurRl8.js";/**
+import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-8hGqMF9G.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.

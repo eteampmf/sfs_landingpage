4eb6c3534ff86160d89ec764356c6efe76127b06
ouwais_local <ouwais@amazon.com>
2025-09-18 08:59:26 +0200

    Updates

--- assets/carousel-Cmq-D3m4.js
+++ assets/carousel-Cmq-D3m4.js
@@ -1,4 +1,4 @@
-import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-8hGqMF9G.js";/**
+import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-2ohkSaOQ.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.

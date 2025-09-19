579512ea2b10f050cea2bce070e3426e327c06c8
ouwais_local <ouwais@amazon.com>
2025-09-19 10:24:05 +0200

    Updates

--- assets/carousel-BVYK2Axc.js
+++ assets/carousel-BVYK2Axc.js
@@ -1,4 +1,4 @@
-import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-BHp_PEUB.js";/**
+import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-DR0IxUs5.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.

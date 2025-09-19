390032d427d27d95e7ba46b39f83348ac969c014
ouwais_local <ouwais@amazon.com>
2025-09-19 10:14:37 +0200

    Updates

--- assets/carousel-CLtWkWDe.js
+++ assets/carousel-CLtWkWDe.js
@@ -1,4 +1,4 @@
-import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-DbjlGlzC.js";/**
+import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-BHp_PEUB.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.

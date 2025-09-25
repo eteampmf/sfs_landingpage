6d36db7a3598d8550879f77562d93beb0957557a
ouwais_local <ouwais@amazon.com>
2025-09-25 10:14:28 +0200

    Updates

--- assets/carousel-DOk3LffU.js
+++ assets/carousel-DOk3LffU.js
@@ -1,4 +1,4 @@
-import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-D5NDrA_V.js";/**
+import{c as Ut,r as N,j as V,a as gt,B as _t}from"./index-CYoUbr_f.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.
